22e0438a9a8ff6fac963582b4ed1cbef9f5a94e8
Thomagotchi-PC <thomassano87@gmail.com>
2024-07-04 23:56:58 +0200

    Ajout des composants pour la page A Propos avec .map

--- src/Pages/APropos/PageAPropos.jsx
+++ src/Pages/APropos/PageAPropos.jsx
@@ -1,11 +1,22 @@
-import BackgroundImage from "../../Assets/Images/vallée.jpg"
-import Banner from "../../Components/Banner/Banner"
-import Dropdown from "../../Components/Dropdown/Dropdown"
-import './PageAPropos.scss';
+import BackgroundImage from "../../Assets/Images/vallée.jpg";
+import Banner from "../../Components/Banner/Banner";
+import Dropdown from "../../Components/Dropdown/Dropdown";
+import "./PageAPropos.scss";
+import Data from "../../Data/APropos.json";
 
-export function PageAPropos () {
-    return <div className="body-container">
-        <Banner img={BackgroundImage} />
-        <Dropdown title={'Équipements'} info={'hi'}/>
+export function PageAPropos() {
+  return (
+    <div className="body-container">
+      <Banner img={BackgroundImage} />
+      <ul className="dropdown-container">
+        {Data.map((element) => (
+          <Dropdown
+            title={element.title}
+            info={element.content}
+            key={element.title}
+          />
+        ))}
+      </ul>
     </div>
+  );
 }
