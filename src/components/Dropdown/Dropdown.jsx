aa44b24af282a41d65ba6cf2bd035b215f8a7003
Thomagotchi-PC <thomassano87@gmail.com>
2024-07-05 19:30:10 +0200

    Transition page A Propos + JSX

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react"
-import arrow from "../../Assets/Arrows/arrow.svg"
-import "./Dropdown.scss"
+import { useState } from "react";
+import arrow from "../../Assets/Arrows/arrow.svg";
+import "./Dropdown.scss";
 
-const Dropdown = ({title, info}) => {
-    const [active, setActive] = useState(true)
+const Dropdown = ({ title, info }) => {
+  const [active, setActive] = useState(true);
 
-    function toggleActive() {
-        setActive(!active)
-    }
+  function toggleActive() {
+    setActive(!active);
+  }
 
-    return <div className="dropdown-box">
-        <div className="dropdown-top-bar">
-            <h2 className="dropdown-title">{title}</h2>
-            <img src={ arrow } alt="dropdown arrow" onClick={toggleActive} className={`dropdown-arrow-${active}`}/>
-        </div>
-        <div className={`drowndown-info-${active}`}>
-            <p className="dropdown-info-text">{info}</p>
-        </div>
-    </div>
-}
+  return (
+    <li className="dropdown-box">
+      <div className="dropdown-top-bar">
+        <h2 className="dropdown-title">{title}</h2>
+        <img
+          src={arrow}
+          alt="dropdown arrow"
+          onClick={toggleActive}
+          className={`dropdown-arrow-${active}`}
+        />
+      </div>
+      <div className={`dropdown-info-${active}`}>
+        <p className="dropdown-info-text">{info}</p>
+      </div>
+    </li>
+  );
+};
 
-export default Dropdown
+export default Dropdown;
