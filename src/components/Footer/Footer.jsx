c6a40e63add4b5b944c47cdf9fa90a9784b7cefc
Thomagotchi-PC <thomassano87@gmail.com>
2024-07-09 14:02:50 +0200

    Date footer dynamique

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,16 @@
-import logo from '../../Assets/Logos/white-logo.svg';
-import './Footer.scss';
+import logo from "../../Assets/Logos/white-logo.svg";
+import "./Footer.scss";
 
-export function PageFooter () {
-    
-    return <footer>
-                <div className='footer-container'>
-                    <img src={logo} alt="Logo Kasa blanc" />
-                    <p>© 2020 Kasa. All rights reserved</p>
-                </div>
-        </footer>
-  }
+export function PageFooter() {
+  const currentDate = new Date();
+  const getDate = currentDate.getFullYear();
+
+  return (
+    <footer>
+      <div className="footer-container">
+        <img src={logo} alt="Logo Kasa blanc" />
+        <p>© {getDate} Kasa. All rights reserved</p>
+      </div>
+    </footer>
+  );
+}
