baeefdb8ca04b4724ce7b522f1b644e8d8f3e0cf
Thomagotchi <thomassano87@gmail.com>
2024-07-11 10:18:58 +0200

    Page Erreur Responsive + Carousel Responsive debug

--- src/Pages/Error/PageError.jsx
+++ src/Pages/Error/PageError.jsx
@@ -1,10 +1,15 @@
-import { Link } from 'react-router-dom'
-import './PageError.scss';
+import { Link } from "react-router-dom";
+import "./PageError.scss";
 
-export function PageError () {
-    return <div className="error-container">
-        <h1>404</h1>
-        <h2>Oups! La page que vous demandez n'existe pas.</h2>
-        <Link to="/">Retournez sur la page d'acceuil</Link>
+export function PageError() {
+  return (
+    <div className="error-container">
+      <h1>404</h1>
+      <div className="error-info-container">
+        <h2>Oups! La page que</h2>
+        <h2>vous demandez n'existe pas.</h2>
+      </div>
+      <Link to="/">Retournez sur la page d'acceuil</Link>
     </div>
+  );
 }
