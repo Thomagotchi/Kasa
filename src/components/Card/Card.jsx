14935976f1deefd3221f5151739a918ce15ae2ad
Thomagotchi <thomassano87@gmail.com>
2024-07-12 11:21:24 +0200

    Commentaires Banner + Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,20 @@
+//--- Import de la fonction Link de React ---
 import { Link } from "react-router-dom";
+
+//--- Import du CSS ---
 import "./Card.scss";
 
+//--- Composant Card pour la page Acceuil ---
 const Card = ({ id, title, cover }) => {
   return (
+    //--- La carte renvoie l'ID du logement en paramètres URL et puis renvoie l'utilisateur à la fonction 'Redirectlogement' ---
     <Link to={id} className="gallery-card">
+      {/*--- Image de la carte ---*/}
       <div className="cover-container">
         <img src={cover} alt={title} className="gallery-image" />
       </div>
+
+      {/*--- Titre de la carte ---*/}
       <div className="card-title-container">
         <h2 className="card-title">{title}</h2>
       </div>
